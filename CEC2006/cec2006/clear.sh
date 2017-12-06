8100f9a2f459ed854c52b99572df771392bcdc76
alaya <lvwenlong_lambda@qq.com>
2017-12-06 11:33:46 +0800

    Remove CEC2006

--- CEC2006/cec2006/clear.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rvf weibo.*.log
-rm -rvf world.*.log
-rm -rvf master.world.*.log
-rm -rvf opt/ conf log err
--- CEC2006/cec2006/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-./clear.sh
-dir=`pwd`
-echo workdir $dir > conf
-cat ./tmp.conf >> conf
-nohup mace_bo ./conf > log 2>&1 &
